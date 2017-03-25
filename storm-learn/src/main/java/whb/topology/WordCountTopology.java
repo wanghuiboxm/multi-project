34b628661393b96efba4fbc74f7c0225f037ea1e
wanghuibo <wanghuibo@xiaomi.com>
2017-03-25 23:16:23 +0800

    init commit

---
+++ storm-learn/src/main/java/whb/topology/WordCountTopology.java
@@ -0,0 +1,76 @@
+package whb.topology;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Created by wanghb on 2017/3/25.
+ */
+public class WordCountTopology {
+    public static class RandomSpout extends BaseRichSpout {
+        private SpoutOutputCollector collector;
+        private static String[] words = new String[]{"happy", "hellword", "firtStorm"};
+
+        @Override
+        public void open(Map map, TopologyContext topologyContext, SpoutOutputCollector spoutOutputCollector) {
+            this.collector = spoutOutputCollector;
+        }
+
+        @Override
+        public void nextTuple() {
+            String word = words[new Random().nextInt(words.length)];
+            collector.emit(new Values(word));
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+            outputFieldsDeclarer.declare(new Fields("randomstring"));
+        }
+    }
+
+    public static class SenqueceBolt extends BaseBasicBolt {
+        @Override
+        public void execute(Tuple tuple, BasicOutputCollector basicOutputCollector) {
+            String word = tuple.getString(0);
+            String out = "word is " + word;
+            System.out.println(out);
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("spout", new RandomSpout());
+        builder.setBolt("bolt", new SenqueceBolt()).localOrShuffleGrouping("spout");
+        Config config = new Config();
+        if(args != null && args.length > 0) {
+            config.setNumWorkers(3);
+            StormSubmitter.submitTopology(args[0], config, builder.createTopology());
+        } else {
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("firsttopology", config, builder.createTopology());
+            Utils.sleep(5000);
+            cluster.killTopology("firsttopology");
+            cluster.shutdown();
+        }
+    }
+}
